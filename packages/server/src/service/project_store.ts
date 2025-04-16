740f5469cc8e28931c3207b65ed1563c64e7b67d
Kyle Nesbit <kjnesbit@gmail.com>
2025-04-16 07:46:54 -0600

    Fix project listing.
    Signed-off-by: Kyle Nesbit <kjnesbit@gmail.com>

--- packages/server/src/service/project_store.ts
+++ packages/server/src/service/project_store.ts
@@ -13,23 +13,25 @@ export class ProjectStore {
     }
 
     public async listProjects(): Promise<ApiProject[]> {
-        const projectManifestJson = await ProjectStore.getProjectManifest(this.serverRootPath);
-        return Object.keys(projectManifestJson.projects).map((projectName) => ({
-            name: projectName,
-        })) as ApiProject[];
+        const projectManifest = await ProjectStore.getProjectManifest(this.serverRootPath);
+        if (projectManifest.projects === undefined) {
+            return [];
+        } else {
+            return Object.keys(projectManifest.projects).map((projectName) => ({
+                name: projectName,
+            })) as ApiProject[];
+        }
     }
 
     public async getProject(projectName: string): Promise<Project> {
         let project = this.projects.get(projectName);
         if (project === undefined) {
-            const projectManifestJson = await ProjectStore.getProjectManifest(this.serverRootPath);
-
-            if (projectManifestJson.projects[projectName] === undefined) {
+            const projectManifest = await ProjectStore.getProjectManifest(this.serverRootPath);
+            if (projectManifest.projects[projectName] === undefined) {
                 throw new Error(`Project ${projectName} not found in publisher.config.json`);
             }
-
             project = await Project.create(
-                path.join(this.serverRootPath, projectManifestJson.projects[projectName]),
+                path.join(this.serverRootPath, projectManifest.projects[projectName]),
             );
             this.projects.set(projectName, project);
         }
@@ -39,7 +41,11 @@ export class ProjectStore {
     private static async getProjectManifest(
         serverRootPath: string,
     ): Promise<{ projects: { [key: string]: string } }> {
-        const projectManifest = await fs.readFile(path.join(serverRootPath, "publisher.config.json"), "utf8");
-        return JSON.parse(projectManifest);
+        try {
+            const projectManifest = await fs.readFile(path.join(serverRootPath, "publisher.config.json"), "utf8");
+            return JSON.parse(projectManifest);
+        } catch (error) {
+            return { projects: {} };
+        }
     }
 }
