e9148a9f43af09c6427772a1eab24f644e763d5e
Josh Sacks <josh.sacks@gmail.com>
2025-05-01 10:42:37 -0600

    Only update SDK resolution in dev mode
    Signed-off-by: Josh Sacks <josh.sacks@gmail.com>

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -4,16 +4,18 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
+   const isDev = mode === "development";
+      // In Dev, Resolve the SDK locally as src, not /dist so it can hot reload
+      const resolve = isDev ? {
+      alias: {
+         '@malloy-publisher/sdk': path.resolve(__dirname, '../sdk/src'),
+      },
+   } : undefined;
    return defineConfig({
       plugins: [react()],
       define: {
          "process.env": JSON.stringify(mode),
       },
-      // Resolve the SDK locally as src, not /dist so it can hot reload
-      resolve: {
-         alias: {
-             '@malloy-publisher/sdk': path.resolve(__dirname, '../sdk/src'),
-         },
-     },
+      resolve
    });
 };
