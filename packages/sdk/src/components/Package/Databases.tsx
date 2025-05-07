ee78e80e23e7ff1cb1e9688e2c3e093140b095e5
Josh Sacks <josh.sacks@gmail.com>
2025-05-07 09:49:56 -0600

    Revamp database UI, show schemas
    Signed-off-by: Josh Sacks <josh.sacks@gmail.com>

--- packages/sdk/src/components/Package/Databases.tsx
+++ packages/sdk/src/components/Package/Databases.tsx
@@ -1,6 +1,10 @@
 import {
    Box,
+   Dialog,
+   DialogContent,
+   DialogTitle,
    Divider,
+   IconButton,
    Table,
    TableBody,
    TableCell,
@@ -11,8 +15,9 @@ import {
    Typography,
 } from "@mui/material";
 import { QueryClient, useQuery } from "@tanstack/react-query";
-import { Configuration, DatabasesApi } from "../../client";
+import { Configuration, Database, DatabasesApi } from "../../client";
 import { StyledCard, StyledCardContent } from "../styles";
+import React from "react";
 
 const databasesApi = new DatabasesApi(new Configuration());
 const queryClient = new QueryClient();
@@ -25,7 +30,7 @@ interface DatabaseProps {
    accessToken: string;
 }
 
-export default function Database({
+export default function DatabaseView({
    server,
    projectName,
    packageName,
@@ -47,15 +52,15 @@ export default function Database({
       },
       queryClient,
    );
-   const formatFileSize = (size: number) => {
+   const formatRowSize = (size: number) => {
       if (size >= 1024 * 1024 * 1024 * 1024) {
-         return `${(size / (1024 * 1024 * 1024)).toFixed(2)} TB`;
+         return `${(size / (1024 * 1024 * 1024)).toFixed(2)} T`;
       } else if (size >= 1024 * 1024 * 1024) {
-         return `${(size / (1024 * 1024 * 1024)).toFixed(2)} GB`;
+         return `${(size / (1024 * 1024 * 1024)).toFixed(2)} G`;
       } else if (size >= 1024 * 1024) {
-         return `${(size / (1024 * 1024)).toFixed(2)} MB`;
+         return `${(size / (1024 * 1024)).toFixed(2)} M`;
       } else {
-         return `${(size / 1024).toFixed(2)} KB`;
+         return `${(size / 1024).toFixed(2)} K`;
       }
    };
    return (
@@ -86,33 +91,18 @@ export default function Database({
                         <TableHead>
                            <TableRow>
                               <TableCell>Database Name</TableCell>
-                              <TableCell align="right">Size</TableCell>
+                              <TableCell align="right">rows</TableCell>
                            </TableRow>
                         </TableHead>
                         <TableBody>
                            {data.data.map((database) => (
                               <TableRow key={database.path}>
                                  <TableCell>
-                                    <Tooltip
-                                       title={database.path}
-                                       placement="top"
-                                    >
-                                       <Typography
-                                          variant="body2"
-                                          sx={{
-                                             maxWidth: "200px",
-                                             overflow: "hidden",
-                                             textOverflow: "ellipsis",
-                                             whiteSpace: "nowrap",
-                                          }}
-                                       >
-                                          {database.path}
-                                       </Typography>
-                                    </Tooltip>
+                                    <NameAndSchema database={database} />
                                  </TableCell>
                                  <TableCell align="right">
                                     <Typography variant="body2">
-                                       {formatFileSize(database.size)}
+                                       {formatRowSize(database.info.rowCount)}
                                     </Typography>
                                  </TableCell>
                               </TableRow>
@@ -133,4 +123,83 @@ export default function Database({
          </StyledCardContent>
       </StyledCard>
    );
+}
+
+function NameAndSchema({ database }: { database: Database }) {
+   const [open, setOpen] = React.useState(false);
+   return <Box
+      sx={{ display: 'flex', alignItems: 'center' }}
+      onClick={() => setOpen(!open)}
+   style={{ cursor: 'pointer' }}
+   >
+   
+   <Typography
+      variant="body2"
+      color="primary"
+      sx={{
+         maxWidth: "200px",
+         overflow: "hidden",
+         textOverflow: "ellipsis",
+         whiteSpace: "nowrap",
+      }}
+   >
+      {database.path}
+   </Typography>
+   &nbsp;
+   <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ mr: 1, display: 'flex', alignItems: 'center' }}>
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M9 15L20 4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M15 4H20V9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </Box>
+   </Box>
+ <SchemaButton database={database} open={open} setClose={() => setOpen(false)} />
+</Box>
+}
+
+function SchemaButton({ database, open, setClose }: { open: boolean, setClose: () => void, database: Database }) {
+  return (
+      <Dialog
+         open={open}
+         onClose={setClose}
+         maxWidth="sm"
+         fullWidth
+      >
+      <DialogTitle>
+         Schema: <Typography fontSize="large" variant="body2" fontFamily="monospace" component="span">{database.path}</Typography>
+         <IconButton
+            aria-label="close"
+            onClick={setClose}
+            sx={{ position: 'absolute', right: 8, top: 8 }}
+         >
+            <Box sx={{ width: 24, height: 24, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+               X
+            </Box>
+         </IconButton>
+      </DialogTitle>
+      <DialogContent>
+         <TableContainer>
+         <Table size="small"
+               sx={{ "& .MuiTableCell-root": { padding: "10px" } }}>
+               <TableHead>
+                  <TableRow>
+                     <TableCell>NAME</TableCell>
+                     <TableCell>TYPE</TableCell>
+                  </TableRow>
+               </TableHead>
+               <TableBody>
+                  {database.info.columns.map((row) => (
+                     <TableRow key={row.name}>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell>{row.type}</TableCell>
+                     </TableRow>
+                  ))}
+               </TableBody>
+            </Table>
+         </TableContainer>
+      </DialogContent>
+      </Dialog>
+   )
 }
--- packages/server/src/service/package.ts
+++ packages/server/src/service/package.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs/promises";
 import * as path from "path";
+
 import { components } from "../api";
 import recursive from "recursive-readdir";
 import { PackageNotFoundError } from "../errors";
@@ -11,15 +12,21 @@ import {
 } from "../utils";
 import { Scheduler } from "./scheduler";
 import { metrics } from "@opentelemetry/api";
-import { Connection } from "@malloydata/malloy";
+import {
+   Connection,
+   ConnectionRuntime,
+   EmptyURLReader,
+   SourceDef,
+} from "@malloydata/malloy";
 import { createConnections } from "./connection";
 import { DuckDBConnection } from "@malloydata/db-duckdb";
 import { API_PREFIX } from "../constants";
-
 type ApiDatabase = components["schemas"]["Database"];
 type ApiModel = components["schemas"]["Model"];
 export type ApiPackage = components["schemas"]["Package"];
 type ApiSchedule = components["schemas"]["Schedule"];
+type ApiColumn = components["schemas"]["Column"];
+type ApiTableDescription = components["schemas"]["TableDescription"];
 
 export class Package {
    private packageName: string;
@@ -222,13 +229,14 @@ export class Package {
       return await Promise.all(
          (await Package.getDatabasePaths(packagePath)).map(
             async (databasePath) => {
-               const databaseSize: number = await Package.getDatabaseSize(
+               const databaseInfo = await Package.getDatabaseInfo(
                   packagePath,
                   databasePath,
                );
+
                return {
                   path: databasePath,
-                  size: databaseSize,
+                  info: databaseInfo,
                   type: "embedded",
                };
             },
@@ -248,11 +256,32 @@ export class Package {
          .filter((modelPath: string) => modelPath.endsWith(".parquet"));
    }
 
-   private static async getDatabaseSize(
+   private static async getDatabaseInfo(
       packagePath: string,
       databasePath: string,
-   ): Promise<number> {
+   ): Promise<ApiTableDescription> {
       const fullPath = path.join(packagePath, databasePath);
-      return (await fs.stat(fullPath)).size;
+
+      const runtime = new ConnectionRuntime({
+         urlReader: new EmptyURLReader(),
+         connections: [new DuckDBConnection("duckdb")],
+      });
+      const model = runtime.loadModel(
+         `source: temp is duckdb.table('${fullPath}')`,
+      );
+      const modelDef = await model.getModel();
+      const fields = (modelDef._modelDef.contents["temp"] as SourceDef).fields;
+      const schema = fields.map((field): ApiColumn => {
+         return { type: field.type, name: field.name };
+      });
+      console.log("schema", schema);
+
+      const runner = model.loadQuery(
+         "run: temp->{aggregate: row_count is count()}",
+      );
+      const result = await runner.run();
+      console.log("results", result.data.value);
+      const rowCount = result.data.value[0].row_count?.valueOf() as number;
+      return { name: databasePath, rowCount, columns: schema };
    }
 }
