da81e85b668e159c9cab7ed5d970afce53519bf2
Kyle Nesbit <kjnesbit@gmail.com>
2025-06-10 16:56:09 -0600

    Remove console log
    Signed-off-by: Kyle Nesbit <kjnesbit@gmail.com>

--- packages/server/src/service/model.ts
+++ packages/server/src/service/model.ts
@@ -308,7 +308,6 @@ export class Model {
                         ? (await cell.runnable.getPreparedResult())
                            .resultExplore.limit
                         : ROW_LIMIT;
-                     console.log("rowLimit", rowLimit);
                      const result = await cell.runnable.run({ rowLimit });
                      const query = (await cell.runnable.getPreparedQuery())
                         ._query;
