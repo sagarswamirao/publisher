4a46e996a883d944678f8edd29c95673b9100d82
Matt Wilder <mattwilder@gmail.com>
2025-06-26 21:44:46 -0600

    Fix to patch Node env reference and proxy to avoid CORS issue.

--- examples/data-app/vite.config.ts
+++ examples/data-app/vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+    process: { env: {} }, // or a more specific mock
+  },
+  server: {
+    proxy: {
+      "/api/v0": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
