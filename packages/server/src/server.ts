466802c81c4a9ea7ab595ff6468b4655a9311ac9
Sagar Swami Rao Kulkarni <sagarswamirao@gmail.com>
2025-08-25 11:04:47 -0600

    Formatting

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -314,9 +314,7 @@ app.get(
 app.post(`${API_PREFIX}/connections/test`, async (req, res) => {
    try {
       const connectionStatus =
-         await connectionController.testConnectionConfiguration(
-            req.body,
-         );
+         await connectionController.testConnectionConfiguration(req.body);
       res.status(200).json(connectionStatus);
    } catch (error) {
       logger.error(error);
