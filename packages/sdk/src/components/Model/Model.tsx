14e2be20ed6bcedd15252ce5692f47d694b678c7
Kyle Nesbit <kjnesbit@ms2.co>
2025-08-29 22:40:56 -0600

    Clean up model explorer
    Signed-off-by: Kyle Nesbit <kjnesbit@ms2.co>

--- packages/sdk/src/components/Model/Model.tsx
+++ packages/sdk/src/components/Model/Model.tsx
@@ -1,11 +1,12 @@
 import { ApiErrorDisplay } from "../ApiErrorDisplay";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "@malloydata/malloy-explorer/styles.css";
 import { QueryExplorerResult } from "./SourcesExplorer";
 import { Loading } from "../Loading";
 import { ModelExplorer } from "./ModelExplorer";
 import { ModelExplorerDialog } from "./ModelExplorerDialog";
+import { ModelCell } from "./ModelCell";
 import { useModelData } from "./useModelData";
 import React from "react";
 
@@ -20,7 +21,7 @@ interface ModelProps {
 // import "@malloy-publisher/sdk/malloy-explorer.css";
 
 export default function Model({ modelPath, versionId, onChange }: ModelProps) {
-   const { isError, isLoading, error } = useModelData(modelPath, versionId);
+   const { data, isError, isLoading, error } = useModelData(modelPath, versionId);
    const [dialogOpen, setDialogOpen] = React.useState(false);
    const [sharedQuery, setSharedQuery] = React.useState<
       QueryExplorerResult | undefined
@@ -57,39 +58,94 @@ export default function Model({ modelPath, versionId, onChange }: ModelProps) {
             paddingTop: "24px",
          }}
       >
-         <ModelExplorer
-            modelPath={modelPath}
-            versionId={versionId}
-            onChange={handleQueryChange}
-            onSourceChange={handleSourceChange}
-            existingQuery={sharedQuery}
-            initialSelectedSourceIndex={sharedSourceIndex}
-         />
+         {/* Sources Section */}
+         {Array.isArray(data?.sourceInfos) && data.sourceInfos.length > 0 && (
+            <Stack spacing={2} component="section">
+               {/* Sources Header */}
+               <Box sx={{ padding: "0 0 16px 0" }}>
+                  <Typography
+                     variant="h1"
+                     sx={{
+                        fontSize: "28px",
+                        fontWeight: "600",
+                        color: "#1a1a1a",
+                        marginBottom: "8px",
+                        marginTop: "0",
+                        paddingLeft: "0",
+                     }}
+                  >
+                     Sources
+                  </Typography>
+               </Box>
 
-         {/* Magnifying glass icon */}
-         <IconButton
-            sx={{
-               position: "absolute",
-               top: "90px",
-               right: "4px",
-               backgroundColor: "rgba(255, 255, 255, 0.9)",
-               "&:hover": {
-                  backgroundColor: "rgba(255, 255, 255, 1)",
-               },
-               width: "32px",
-               height: "32px",
-               zIndex: 2,
-            }}
-            onClick={() => setDialogOpen(true)}
-         >
-            <SearchIcon sx={{ fontSize: "18px", color: "#666666" }} />
-         </IconButton>
+               <ModelExplorer
+                  modelPath={modelPath}
+                  data={data}
+                  onChange={handleQueryChange}
+                  onSourceChange={handleSourceChange}
+                  existingQuery={sharedQuery}
+                  initialSelectedSourceIndex={sharedSourceIndex}
+               />
+
+               {/* Magnifying glass icon */}
+               <IconButton
+                  sx={{
+                     position: "absolute",
+                     top: "90px",
+                     right: "4px",
+                     backgroundColor: "rgba(255, 255, 255, 0.9)",
+                     "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 1)",
+                     },
+                     width: "32px",
+                     height: "32px",
+                     zIndex: 2,
+                  }}
+                  onClick={() => setDialogOpen(true)}
+               >
+                  <SearchIcon sx={{ fontSize: "18px", color: "#666666" }} />
+               </IconButton>
+            </Stack>
+         )}
+
+         {/* Named Queries Section */}
+         {data?.queries?.length > 0 && (
+            <Stack spacing={2} component="section" sx={{ marginTop: "24px" }}>
+               {/* Named Queries Header */}
+               <Box sx={{ padding: "0 0 16px 0" }}>
+                  <Typography
+                     variant="h2"
+                     sx={{
+                        fontSize: "24px",
+                        fontWeight: "600",
+                        color: "#1a1a1a",
+                        marginBottom: "0",
+                        marginTop: "8px",
+                        paddingLeft: "0",
+                     }}
+                  >
+                     Named Queries
+                  </Typography>
+               </Box>
+
+               {/* Render the named queries */}
+               {data.queries.map((query) => (
+                  <ModelCell
+                     key={query.name}
+                     modelPath={modelPath}
+                     queryName={query.name}
+                     annotations={query.annotations}
+                  />
+               ))}
+            </Stack>
+         )}
 
          {/* Model Explorer Dialog */}
          <ModelExplorerDialog
             open={dialogOpen}
             onClose={() => setDialogOpen(false)}
             modelPath={modelPath}
+            data={data}
             title={`Model: ${modelPath.split("/").pop()}`}
             existingQuery={sharedQuery}
             initialSelectedSourceIndex={sharedSourceIndex}
