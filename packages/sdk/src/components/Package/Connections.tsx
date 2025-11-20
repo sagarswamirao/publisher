be7cfa9d031fb2a9c29b686c4b1fd9c87bd28b1a
Jarnail Singh <34484056+jellysaini@users.noreply.github.com>
2025-11-20 10:00:04 +0530

    Publisher: Do not allow deleting connections that are in use by a package #442 Reopen (#499)
    * fix issuer
    
    * remove console

--- packages/sdk/src/components/Package/Connections.tsx
+++ packages/sdk/src/components/Package/Connections.tsx
@@ -121,6 +121,7 @@ export default function Connections({ resourceUri }: ConnectionsProps) {
       projectName: projectName,
       connectionName: selectedConnection,
    });
+
    const { data, isSuccess, isError, error } = useQueryWithApiError({
       queryKey: ["connections", projectName],
       queryFn: () => apiClients.connections.listConnections(projectName),
@@ -275,15 +276,11 @@ export default function Connections({ resourceUri }: ConnectionsProps) {
                                     updateConnection.mutateAsync(payload)
                                  }
                                  onDelete={(payload) => {
-                                    if (
-                                       !selectedConnectionResourceUri.startsWith(
-                                          "publisher:",
-                                       )
-                                    ) {
+                                    if (!conn.resource) {
                                        deleteConnection.mutateAsync(payload);
                                     } else {
                                        setNotificationMessage(
-                                          "Cannot delete this connection (publisher: resource)",
+                                          "Cannot delete this connection",
                                        );
                                     }
                                  }}
